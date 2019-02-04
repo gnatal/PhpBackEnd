8ab70f97678ac7c47788ebda410ec79f78300653
Guilherme Victor Ramalho Natal <guilherme.natal@cdt-unb.br>
2019-02-04 15:02:51 -0200

    removing key

--- Includes/Headers.php
+++
@@ -1,7 +0,0 @@
-<?php
-require_once("../Includes/Database.php");
-require_once("../Includes/PHPMailer.php");
-header('Content-type: text/html; charset=utf-8');    
-header('Access-Control-Allow-Origin: *');
-
-?>
